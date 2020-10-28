10254c2b03cb522097ad7582411add801098bd02
zenraiching <rickdavidsangalang@gmail.com>
2020-10-28 10:37:26 +0800

    add

--- src/ProjecTab/Project.js
+++ src/ProjecTab/Project.js
@@ -1,25 +1,30 @@
 import React from 'react';
+
 function Project() {
     
     return(
     <main>
-        <div className="grid">
+        <div className="grid-3">
         
-        
+        <Cards title="fdfdf" content="fdfdfdf"/>
+        <Cards title="fdfdf" content="fdfdfdf"/>
+        <Cards title="fdfdf" content="fdfdfdf"/>
+        <Cards title="fdfdf" content="fdfdfdf"/>
         </div>
     </main>
     );
 }
 
-function Cards(title, content) {
-
-    <div class="card">
+function Cards({title, content}) {
+    return(
+    <div class="card-3">
     <div class="text">
 <div class="card-head">{title}</div>
 <p>{content}</p>
      
   </div>
   </div>
+    );
 }
 
 
