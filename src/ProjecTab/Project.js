c762fad77ef5c49663431167dce60e58c881112f
zenraiching <rickdavidsangalang@gmail.com>
2020-10-28 11:04:55 +0800

    add

--- src/ProjecTab/Project.js
+++ src/ProjecTab/Project.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function Project() {
-    
+    // const [images] = useState([
+    //     {image:require('./image/head-1.jpeg')}
+    // ]);
     return(
     <main>
         <div className="grid-3">
         
-        <Cards title="fdfdf" content="fdfdfdf"/>
+        <Cards title="fdfdf" content="fdfdfdf" />
         <Cards title="fdfdf" content="fdfdfdf"/>
         <Cards title="fdfdf" content="fdfdfdf"/>
         <Cards title="fdfdf" content="fdfdfdf"/>
@@ -18,6 +20,7 @@ function Project() {
 function Cards({title, content}) {
     return(
     <div class="card-3">
+    <img src={'image/head-1.jpeg'} alt="imagge"/>
     <div class="text">
 <div class="card-head">{title}</div>
 <p>{content}</p>
