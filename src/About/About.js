1bad46c91a60f4983d3fbc833e118989fa4d6433
zenraiching <rickdavidsangalang@gmail.com>
2020-11-01 10:09:48 +0800

    created about page

--- src/About/About.js
+++ src/About/About.js
@@ -3,23 +3,41 @@ function About() {
     return(
         <main>
             <div className="about-grid">
-                <div className="about-card INTRO">
-                    <div className="about-section-title">About me</div>
-                    <div className="about-content">
-                    Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                    </div>
+            <div className="about-card INTRO">
+                <div className="about-section-title">About me</div>
+                <div className="about-content">
+                Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                 </div>
-                <div className="about-card EDUCATION">
-                    <div className="about-section-title">About me</div>
-                    <div className="about-content">
-                        <div className="education-card">
-                            <div className="school-year">2016-2018</div>
-                            <div className="name-school">STI College Angeles</div>
-                            <div className="field-study">Information Communication Technology</div>
-                        </div>
+            </div>
+            <div className="about-card EDUCATION">
+                <div className="about-section-title">About me</div>
+                <div className="about-content-grid">
+                    <div className="education-card">
+                        <div className="school-year">2016-2018</div>
+                        <div className="name-school">STI College Angeles</div>
+                        <div className="field-study">Information Communication Technology</div>
+                    </div>
 
+                    <div className="education-card">
+                        <div className="school-year">2016-2018</div>
+                        <div className="name-school">STI College Angeles</div>
+                        <div className="field-study">Information Communication Technology</div>
                     </div>
+
+                    <div className="education-card">
+                        <div className="school-year">2016-2018</div>
+                        <div className="name-school">STI College Angeles</div>
+                        <div className="field-study">Information Communication Technology</div>
+                    </div>
+
+                    <div className="education-card">
+                        <div className="school-year">2016-2018</div>
+                        <div className="name-school">STI College Angeles</div>
+                        <div className="field-study">Information Communication Technology</div>
+                    </div>
+
                 </div>
+            </div>
             </div>
         </main>
     );
