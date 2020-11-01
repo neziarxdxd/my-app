4a0eceab9905eb49a2f67e265f1007161d04cee0
zenraiching <rickdavidsangalang@gmail.com>
2020-11-02 00:15:53 +0800

    add

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import Project from './ProjecTab/Project.js';
 import ContactMe from './ContactMe/ContactMe.js';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import About from './About/About';
+import Footer from './Footer/Footer';
 function App() {
   return (
     <Router>
-        <NavBar/>
+       
     <div className="App">
-    
+      <NavBar/>
      <Switch>
        <Route path="/main" component={MainTab}/>
        <Route path="/project" component={Project}/>
        <Route path="/contact" component={ContactMe}/>
        <Route path="/about" component={About}/>
-     </Switch>
+       
+      </Switch>
+      <Footer/>
 
-    </div>
+    
+    </div> 
     </Router>
+  
   );
 }
 
