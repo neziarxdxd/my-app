a6b1c7a4130bfa9153fa77bd534a85720fdb6827
zenraiching <rickdavidsangalang@gmail.com>
2020-11-01 23:42:18 +0800

    adding footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Footer() {
+    return(
+        <div className="footer">
+            <div>
+                Testing only
+            </div>
+        </div>
+
+        
+    );
+}
+export default Footer;
