277d4a20fa3e243ebfc8594ab1a575290e71dfa7
zenraiching <rickdavidsangalang@gmail.com>
2020-11-05 14:42:50 +0800

    adding readme

--- src/ProjecTab/PageProject.js
+++ src/ProjecTab/PageProject.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactMarkdown from 'react-markdown'
+
 function PageProject() {
     return(
-        <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+        <main>
+        
+        <Terms/>
+        </main>
     );
 }
 
+
+class Terms extends Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = { terms: null }
+    }
+  
+    componentWillMount() {
+      fetch(`https://raw.githubusercontent.com/zenraiching/TollGateJava/master/README.md`).then((response) => response.text()).then((text) => {
+        this.setState({ terms: text })
+      })
+    }
+  
+    render() {
+      return (
+        <div className="content">
+          <ReactMarkdown source={this.state.terms} />
+        </div>
+      )
+    }
+  }
+  
 export default PageProject;
