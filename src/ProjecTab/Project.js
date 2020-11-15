5a42524b1b14ca66d1e3840162a7e371be769ef0
zenraiching <rickdavidsangalang@gmail.com>
2020-11-15 18:27:42 +0800

    add

--- src/ProjecTab/Project.js
+++ src/ProjecTab/Project.js
@@ -1,12 +1,13 @@
 import listOfProjects from './listOfProjects.json';
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 function Project() {
 
     const cards= listOfProjects.map((data)=>
-        
-        <Cards title={data.project_name} content={data.description} image={data.image}/>
+       
+        <Cards title={data.project_name} content={data.description} image={data.image} project_name={data.project_name}/>
+   
     );
    
 
@@ -20,10 +21,10 @@ function Project() {
         </main>
     );
 }
-function Cards({title, content,image}) {
+function Cards({title, content,image,project_name}) {
     return(
         <div class="card-3">
-
+            
             <img alt="LORD" src={image}/>
 
             <div class="text-content row-grid">
@@ -34,20 +35,21 @@ function Cards({title, content,image}) {
                 </div>
                 <div class="project-section row-3"> 
                 <div class="icons-only">
-
+                <Link to={`page/${project_name}`}href="/raizen" className="project-icon">
+                    <svg  stroke-width="0" viewBox="0 0 512 512" size="50" aria-hidden="true" focusable="false" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M405.34 405.332H106.66V106.668H240V64H106.66C83.191 64 64 83.197 64 106.668v298.664C64 428.803 83.191 448 106.66 448h298.68c23.469 0 42.66-19.197 42.66-42.668V272h-42.66v133.332zM288 64v42.668h87.474L159.999 322.133l29.866 29.866 215.476-215.47V224H448V64H288z"></path></svg>   
+                    </Link>
+                    
                     <a href="/raizen" className="project-icon">    
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
                     </a>
 
-                    <a href="/raizen" className="project-icon">
-                    <svg  stroke-width="0" viewBox="0 0 512 512" size="50" aria-hidden="true" focusable="false" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M405.34 405.332H106.66V106.668H240V64H106.66C83.191 64 64 83.197 64 106.668v298.664C64 428.803 83.191 448 106.66 448h298.68c23.469 0 42.66-19.197 42.66-42.668V272h-42.66v133.332zM288 64v42.668h87.474L159.999 322.133l29.866 29.866 215.476-215.47V224H448V64H288z"></path></svg>   
-                    </a>
-                    
+                   
                 </div>
 
             </div>
             
             </div>
+           
         </div>
     );
 }
